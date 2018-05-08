5de6fc52908e05775ac49507383536ae045f7015
Dedli <dedli888@gmail.com>
2018-05-08 20:47:07 +0300

    add button to top

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -74,3 +74,22 @@ function displayNoneDropdownArea() {
     header.style.borderBottom = "none";
 }
 
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+const btnToTop = document.getElementById("btn-to-top")
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+       btnToTop.style.display = "block";
+    } else {
+        btnToTop.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+btnToTop.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
